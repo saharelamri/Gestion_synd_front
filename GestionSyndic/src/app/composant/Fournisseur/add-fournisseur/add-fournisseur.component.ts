4fcc17b7447f7493718cc23d709c8f00418ef9a2
takwamn <50061382+takwamn@users.noreply.github.com>
2020-05-05 04:09:50 +0100

    gestion fournisseur

--- GestionSyndic/src/app/composant/Fournisseur/add-fournisseur/add-fournisseur.component.ts
+++ GestionSyndic/src/app/composant/Fournisseur/add-fournisseur/add-fournisseur.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Fournisseur } from 'src/app/models/fournisseur';
+import { FournisseursService } from 'src/app/service/fournisseur.service';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-add-fournisseur',
   templateUrl: './add-fournisseur.component.html',
   styleUrls: ['./add-fournisseur.component.css']
 })
 export class AddFournisseurComponent implements OnInit {
+// constructor() { }
 
-  constructor() { }
+public currentFournisseur:any;
+mode:number=1;
+id:number;
+constructor(private fournisseursService : FournisseursService, private router : Router ) { }
 
-  ngOnInit(): void {
-  }
+ngOnInit(): void {
+}
+
+OnsaveFournisseur(data:Fournisseur){
+  this.fournisseursService.createFournisseur(data).subscribe(res=>{
+    this.currentFournisseur=res
+    this.mode=2;
+    this.gotolist();
+    }
+    ,err=>{console.log(err);
+   })
+
+ } 
+gotolist(){
+ this.router.navigateByUrl("fournisseurs/lister");
+}
+/*OngetCurrentFournisseur(id: number){
+ id==this.currentFournisseur.getIdF();
+ // if(this.mode=2)
+ this.fournisseursService.getFournisseur(id).subscribe(res=>{
+   this.currentFournisseur=res;
+   })
+}*/
+OnewFournisseur(){
+  this.mode=1;
+}
+
 
 }
+
--- GestionSyndic/src/app/composant/Fournisseur/update-fournisseur/update-fournisseur.component.ts
+++ GestionSyndic/src/app/composant/Fournisseur/update-fournisseur/update-fournisseur.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute, Router } from '@angular/router';
+import { Fournisseur } from 'src/app/models/fournisseur';
+import { FournisseursService } from 'src/app/service/fournisseur.service';
 @Component({
   selector: 'app-update-fournisseur',
   templateUrl: './update-fournisseur.component.html',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdateFournisseurComponent implements OnInit {
 
-  constructor() { }
+  id : number;
+  fournisseur1: any;
+  
+  constructor(private route:ActivatedRoute, private router: Router ,private fournisseurService : FournisseursService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.ongetFournisseur();
   }
 
-}
+
+  ongetFournisseur(){
+  
+this.fournisseurService.getFournisseur(this.route.snapshot.params.id)
+  .subscribe(data=>
+    this.fournisseur1=data
+  )}
+
+
+ onupdateFournisseur(value:any){
+    
+let fournisseur:Fournisseur = new Fournisseur(this.route.snapshot.params.id,value.nomF,value.contactF,value.adrF,value.emailF,value.siteWebF);
+ 
+   this.fournisseurService.updateFournisseur(this.route.snapshot.params.id,fournisseur).subscribe(data=>
+      this.router.navigateByUrl("/fournisseurs/lister"))}
+
+   }
