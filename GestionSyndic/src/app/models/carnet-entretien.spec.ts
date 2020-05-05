691e90a8cc244938536bbba1f22e049768ef6826
takwamn <50061382+takwamn@users.noreply.github.com>
2020-05-05 03:43:32 +0100

    ini
    ini

---
+++ GestionSyndic/src/app/models/carnet-entretien.spec.ts
@@ -0,0 +1,7 @@
+import { CarnetEntretien } from './carnet-entretien';
+
+describe('CarnetEntretien', () => {
+  it('should create an instance', () => {
+    expect(new CarnetEntretien()).toBeTruthy();
+  });
+});
